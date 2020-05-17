469e3ada73cd452b6165d7594d6f01fa24a6f8cd
erbynn <john.erbynn@gmail.com>
2020-05-17 19:24:39 +0100

    finish publisher setup

--- learn-delegate-and-events/VideoEncoder.cs
+++ learn-delegate-and-events/VideoEncoder.cs
@@ -5,10 +5,27 @@ namespace learn_delegate_and_events
 {
     public class VideoEncoder
     {
+        // Publisher, publishing an event....
+        // 1. Define delegate
+        // 2. Define event based on that delegate
+        // 3. Raise the event
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);  // 1 => shape of method to be called in subscriber
+
+        public event VideoEncodedEventHandler VideoEncoded;  // 2 
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video");
             Thread.Sleep(3000);
+
+            OnVideoEncoded();  // 3 
+        }
+
+        public virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
         }
     }
 }
