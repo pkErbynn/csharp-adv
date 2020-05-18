4a57d5e29485f2d044e8150b49f9e87fd489dba3
erbynn <john.erbynn@gmail.com>
2020-05-18 08:40:54 +0100

    add Select

--- learn-class-extension/Program.cs
+++ learn-class-extension/Program.cs
@@ -16,11 +16,12 @@ namespace learn_class_extension
             //}
 
             // above can be replaced by linq
-            var cheapBooks = books.Where(b => b.Price < 10).OrderBy(b => b.Price);
-
+            var cheapBooks = books.Where(b => b.Price < 10).OrderBy(b => b.Price).Select(b => b.Title); // Select => map
+             
             foreach(var book in cheapBooks)
             {
-                Console.WriteLine(book.Title + " " + book.Price);
+                //Console.WriteLine(book.Title + " " + book.Price);
+                Console.WriteLine(book);  // only book titles
             }
         }
     }
