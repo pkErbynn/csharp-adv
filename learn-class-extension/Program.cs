779f9558209c5242f66d6bfaf0793354fe2f93f7
erbynn <john.erbynn@gmail.com>
2020-05-18 09:31:56 +0100

    more filter and aggregate fxns

--- learn-class-extension/Program.cs
+++ learn-class-extension/Program.cs
@@ -15,12 +15,6 @@ namespace learn_class_extension
             //        cheapBooks.Add(book);
             //}
 
-            // using LINQ Extention Methods
-            var cheapBooks = books
-                                                .Where(b => b.Price < 10)  // filter
-                                                .OrderBy(b => b.Price)  // sort
-                                                .Select(b => b.Title); //  map
-
             // using LINQ Query Operators
             var cheapBooks2 = 
                               from b in books
@@ -31,8 +25,51 @@ namespace learn_class_extension
             foreach (var book in cheapBooks2)
             {
                 Console.WriteLine(book.Title + " " + book.Price);
-                //Console.WriteLine(book);  // only book titles
             }
+
+
+            // using LINQ Extention Methods
+            var cheapBooks = books
+                                                .Where(b => b.Price < 10)  // filter
+                                                .OrderBy(b => b.Price)  // sort
+                                                .Select(b => b.Title); //  map
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book);
+            }
+
+
+            // other useful filters
+            var oneBook = books.SingleOrDefault(b =>b.Price == 8);  // Default => if not exist, return null
+            Console.WriteLine(oneBook.Title);
+
+            var oneBook2 = books.FirstOrDefault(b => b.Price != 30);
+            Console.WriteLine(oneBook2.Title);
+
+            var oneBook3 = books.LastOrDefault(b => b.Price != 30);
+            Console.WriteLine(oneBook2.Title);
+
+            var pageBooks = books.Skip(2).Take(2);  // skips first 2 and take the next 2....mostly for paging
+            foreach (var pb in pageBooks)
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine(pb.Title);
+            }
+
+
+            // aggregate
+            var count = books.Count();
+            Console.WriteLine("count: " + count);
+
+            var maxPrice = books.Max(b => b.Price);
+            Console.WriteLine("max: " + maxPrice);
+
+            var minPrice = books.Min(b => b.Price);
+            Console.WriteLine("min: " + minPrice);
+
+            var totalPrice = books.Sum(b => b.Price);
+            Console.WriteLine("sum: " + totalPrice);
         }
     }
 }
