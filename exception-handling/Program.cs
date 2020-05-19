88cf50961d54322f81f36338588d8adbd79146a9
erbynn <john.erbynn@gmail.com>
2020-05-19 06:50:06 +0100

    exception occured outside

--- exception-handling/Program.cs
+++ exception-handling/Program.cs
@@ -1,27 +1,42 @@
 ﻿using System;
-
+using System.IO;
 namespace exception_handling
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //try
+            //{
+            //    Calculator calc = new Calculator();
+            //    var result = calc.Divide(3, 0);
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("Cannot be divided by zero");
+            //}
+            //catch (ArithmeticException)
+            //{
+            //    Console.WriteLine("Error occured with arithmetic");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Oops!....something went wrong..." + ex.Message);
+            //}
+
+            var streamReader = new StreamReader(@"C:\Users\Mr. Erbynn\source\repos\learn\exception-handling\file.zip");
             try
             {
-                Calculator calc = new Calculator();
-                var result = calc.Divide(3, 0);
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Cannot be divideb by zero");
-            }
-            catch (ArithmeticException)
-            {
-                Console.WriteLine("Error occured with arithmetic");
+                var content = streamReader.ReadToEnd();
+                throw new Exception("Oops!!");
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Oops!....something went wrong..." + ex.Message);
+                Console.WriteLine("Sorry, expected error occured...");
+            }
+            finally
+            {
+                streamReader.Dispose();
             }
           
         }
