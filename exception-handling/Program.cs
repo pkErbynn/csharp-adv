71b392044e2ccd8b3c91254bfc651a28efe90eb8
erbynn <john.erbynn@gmail.com>
2020-05-19 06:25:23 +0100

    change assembly/dll names

---
+++ exception-handling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
